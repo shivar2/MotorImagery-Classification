8580a6271407e483ba7f7e7293652e935ea6d7c7
shiva <shiva_roshanravan@yahoo.com>
2021-06-20 19:40:16 +0430

    add evaluations file

---
+++ evaluations.py
@@ -0,0 +1,99 @@
+import logging
+from copy import deepcopy
+from time import time
+from typing import Optional, Union
+
+import numpy as np
+from mne.epochs import BaseEpochs
+from sklearn.base import clone
+from sklearn.metrics import get_scorer
+from sklearn.model_selection import (
+    LeaveOneGroupOut,
+    StratifiedKFold,
+    StratifiedShuffleSplit,
+    cross_val_score,
+)
+from sklearn.model_selection._validation import _fit_and_score, _score
+from sklearn.preprocessing import LabelEncoder
+
+from moabb.evaluations.base import BaseEvaluation
+
+
+log = logging.getLogger(__name__)
+
+# Numpy ArrayLike is only available starting from Numpy 1.20 and Python 3.8
+Vector = Union[list, tuple, np.ndarray]
+
+
+class CrossSubjectEvaluation(BaseEvaluation):
+    """Cross Subject evaluation Context.
+
+    Evaluate performance of the pipeline trained on all subjects but one,
+    concatenating sessions.
+
+    """
+
+    def evaluate(self, dataset, pipelines):
+        if not self.is_valid(dataset):
+            raise AssertionError("Dataset is not appropriate for evaluation")
+        # this is a bit akward, but we need to check if at least one pipe
+        # have to be run before loading the data. If at least one pipeline
+        # need to be run, we have to load all the data.
+        # we might need a better granularity, if we query the DB
+        run_pipes = {}
+        for subject in dataset.subject_list:
+            run_pipes.update(self.results.not_yet_computed(pipelines, dataset, subject))
+        if len(run_pipes) != 0:
+
+            # get the data
+            X, y, metadata = self.paradigm.get_data(
+                dataset, return_epochs=self.return_epochs
+            )
+
+            # encode labels
+            le = LabelEncoder()
+            y = y if self.mne_labels else le.fit_transform(y)
+
+            # extract metadata
+            groups = metadata.subject.values
+            sessions = metadata.session.values
+
+            scorer = get_scorer(self.paradigm.scoring)
+
+            # perform leave one subject out CV
+            cv = LeaveOneGroupOut()
+            for train, test in cv.split(X, y, groups):
+
+                subject = groups[test[0]]
+                # now we can check if this subject has results
+                run_pipes = self.results.not_yet_computed(pipelines, dataset, subject)
+
+                # iterate over pipelines
+                for name, clf in run_pipes.items():
+                    t_start = time()
+                    model = deepcopy(clf).fit(X[train], y[train])
+                    duration = time() - t_start
+
+                    # we eval on each session
+                    for session in np.unique(sessions[test]):
+                        ix = sessions[test] == session
+                        score = _score(model, X[test[ix]], y[test[ix]], scorer)
+
+                        nchan = (
+                            X.info["nchan"] if isinstance(X, BaseEpochs) else X.shape[1]
+                        )
+                        res = {
+                            "time": duration,
+                            "dataset": dataset,
+                            "subject": subject,
+                            "session": session,
+                            "score": score,
+                            "n_samples": len(train),
+                            "n_channels": nchan,
+                            "pipeline": name,
+                        }
+
+                        yield res
+
+    def is_valid(self, dataset):
+        return len(dataset.subject_list) > 1
