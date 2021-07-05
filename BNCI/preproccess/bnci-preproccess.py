677d8abdd12a6fb0d74be2cc4a95337998d2dae9
shiva <shiva_roshanravan@yahoo.com>
2021-07-05 23:42:03 +0430

    delete last preprocess
    Previous commit includes Modularize model for cropped (shallow and deep4)

--- BNCI/preproccess/bnci-preproccess.py
+++
@@ -1,8 +0,0 @@
-from Preprocess.preprocess import *
-
-subject_id = 1
-set_mne_path(dataset_name="BNCI", mne_data_path='../../Dataset-Files/mne_data')
-
-dataset = load_data_moabb(dataset_name="BNCI2014001", subject_id=subject_id)
-dataset = basic_preprocess(dataset, low_cut_hz=4., high_cut_hz=38., factor_new=1e-3, init_block_size=1000)
-save_data(dataset, saving_path='../../Dataset-Files/data-file/bnci-raw/', subject_id=subject_id)
