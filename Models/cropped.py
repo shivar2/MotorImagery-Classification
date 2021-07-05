4380862d0affb4d9c1732e57cb1ec4c12628310d
shiva <shiva_roshanravan@yahoo.com>
2021-07-06 00:13:16 +0430

    delete example

--- Models/cropped.py
+++ Models/cropped.py
@@ -240,36 +240,3 @@ def run_model(data_directory, subject_id_list, dataset_name, model_name, save_pa
 
     plot(clf, save_path)
 
-
-# example of BNCI shallow - subject 1
-# main info
-subject_id_list = [1]
-
-# Path to saving models
-# mkdir path to save
-save_path = os.path.join('../../saved_models/BNCI/cropped/shallow/' + str(subject_id_list).strip('[]')) + '/'
-
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
-
-run_model(data_directory='bnci-raw/',
-          subject_id_list=subject_id_list,
-          dataset_name='BNCI',
-          model_name='shallow',
-          save_path=save_path)
-
-# example of HGD deep4 - subject 1
-subject_id_list = [1]
-
-# Path to saving models
-# mkdir path to save
-save_path = os.path.join('../../saved_models/HGD/cropped/deep4/' + str(subject_id_list).strip('[]')) + '/'
-
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
-
-run_model(data_directory='hgd-raw/',
-          subject_id_list=subject_id_list,
-          dataset_name='HGD',
-          model_name='deep4',
-          save_path=save_path)
