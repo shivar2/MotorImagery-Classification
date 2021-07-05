067548ecda90b0a870aebb6f2c99bf6c44e07479
shiva <shiva_roshanravan@yahoo.com>
2021-07-05 23:58:24 +0430

    Add cropped.py for BNCI and HGD
    Add cropped.py for BNCI and HGD
    
    Remove (bnci/ hgd)-cropped-(deep4/ shallow)
    
    didn't delete bnci-cropped-transfer-deep4

--- Models/cropped.py
+++ Models/cropped.py
@@ -259,17 +259,17 @@ run_model(data_directory='bnci-raw/',
           save_path=save_path)
 
 # example of HGD deep4 - subject 1
-# subject_id_list = [1]
-#
-# # Path to saving models
-# # mkdir path to save
-# save_path = os.path.join('../../saved_models/HGD/cropped/deep4/' + str(subject_id_list).strip('[]')) + '/'
-#
-# if not os.path.exists(save_path):
-#     os.makedirs(save_path)
-#
-# run_model(data_directory='hgd-raw/',
-#           subject_id_list=subject_id_list,
-#           dataset_name='HGD',
-#           model_name='deep4',
-#           save_path=save_path)
+subject_id_list = [1]
+
+# Path to saving models
+# mkdir path to save
+save_path = os.path.join('../../saved_models/HGD/cropped/deep4/' + str(subject_id_list).strip('[]')) + '/'
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+run_model(data_directory='hgd-raw/',
+          subject_id_list=subject_id_list,
+          dataset_name='HGD',
+          model_name='deep4',
+          save_path=save_path)
