0fc181d32b4d77dbcd2ded0d747ba3a5d8a539cf
shiva <shiva_roshanravan@yahoo.com>
2021-08-13 03:04:06 +0430

    Add WGanGP-Erik
    Used cwt data

---
+++ BNCI/run-wganGP-erik.py
@@ -0,0 +1,98 @@
+"""
+main base code:
+    https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/wgan_gp/wgan_gp.py
+"""
+from braindecode.datautil.windowers import create_windows_from_events
+from braindecode.datautil.serialization import load_concat_dataset
+
+from models.WGanGPErik import WGANGP
+from Preprocess.MIpreprocess import get_normalized_cwt_data
+
+
+def get_data(data_directory='bnci-raw/', subject_id=1, time_sample=32, low_cut_hz=4., high_cut_hz=38., window_stride_samples=1):
+
+    # Dataset
+    dataset = load_concat_dataset(
+        path='../Dataset-Files/data-file/' + data_directory + str(subject_id),
+        preload=True,
+        target_name=None,
+
+    )
+
+    sfreq = dataset.datasets[0].raw.info['sfreq']
+    assert all([ds.raw.info['sfreq'] == sfreq for ds in dataset.datasets])
+
+    trial_start_offset_samples = int(-0.5 * sfreq)
+
+    pick_channels = ['C3', 'C4']              # For All channels set None
+
+    mapping = {
+        # Select just 'feet' task
+        'feet': 0,
+    }
+
+    windows_dataset = create_windows_from_events(
+        dataset,
+        trial_start_offset_samples=trial_start_offset_samples,
+        trial_stop_offset_samples=0,
+        preload=True,
+        window_size_samples=time_sample,
+        window_stride_samples=window_stride_samples,
+        drop_bad_windows=True,
+        picks=pick_channels,
+        mapping=mapping,
+    )
+    splitted = windows_dataset.split('session')
+    train_set = splitted['session_T']
+
+    n_chans = windows_dataset[0][0].shape[0]
+
+    cwt_data = get_normalized_cwt_data(dataset=train_set,
+                                       low_cut_hz=low_cut_hz,
+                                       high_cut_hz=high_cut_hz,
+                                       n_channels=n_chans,
+                                       windows_time=time_sample)
+
+    return cwt_data, n_chans
+
+
+#########################
+# load data             #
+#########################
+data_directory = 'bnci-raw/'
+subject_id = 1
+
+low_cut_hz = 4.
+high_cut_hz = 40.
+
+time_sample = 500
+window_stride_samples = 467
+
+# cuda, device = detect_device()
+#
+# seed = 20200220  # random seed to make results reproducible
+# set_random_seeds(seed=seed, cuda=cuda)
+
+cwt_data, n_chans = get_data(data_directory=data_directory,
+                             subject_id=subject_id,
+                             time_sample=time_sample,
+                             low_cut_hz=low_cut_hz,
+                             high_cut_hz=high_cut_hz,
+                             window_stride_samples=window_stride_samples)
+
+#########################
+# Running params        #
+#########################
+
+batchsize = 64
+epochs = 2500
+
+net = WGANGP(subject=subject_id,
+             n_epochs=epochs,
+             batch_size=batchsize,
+             time_sample=time_sample,
+             channels=n_chans,
+             sample_interval=window_stride_samples,
+             freq_sample=int(high_cut_hz - low_cut_hz))
+
+net.train(cwt_data)
---
+++ models/WGanGPModels.py
@@ -0,0 +1,60 @@
+import torch.nn as nn
+import numpy as np
+
+
+class Generator(nn.Module):
+    def __init__(self, time_sample=1000, noise=100, channels=3, freq_sample=34):
+        super(Generator, self).__init__()
+
+        self.noise = noise
+        self.time_sample = time_sample
+        self.channels = channels
+        self.freq_sample = freq_sample
+        self.eeg_shape = (self.freq_sample, self.time_sample, self.channels)
+        # img_shape = (opt.channels, opt.img_size, opt.img_size)
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(self.noise, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(self.eeg_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.shape[0], * self.eeg_shape)
+        # img = img.view(-1, self.freq_sample, self.time_sample, self.channels)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self, time_sample=1000, channels=3, freq_sample=34):
+
+        super(Discriminator, self).__init__()
+
+        self.time_sample = time_sample
+        self.channels = channels
+        self.freq_sample = freq_sample
+        self.eeg_shape = (self.channels, self.freq_sample, self.time_sample)
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(self.eeg_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.shape[0], -1)
+        validity = self.model(img_flat)
+        return validity
