a26be96b7a0f960ad1643345b15da3bce97f8829
shiva <shiva_roshanravan@yahoo.com>
2021-08-18 06:28:17 +0430

    generate EEG fake - WGanGP signal
    each fake_dataset has 1 session, 144 epoch (events) (for 500 window sample, bci dataset has 144 trial for each session or run)

--- BNCI/generate-bci-WganGPSignal.py
+++ BNCI/generate-bci-WganGPSignal.py
@@ -1,13 +1,14 @@
 
 import os
+import random
 import numpy as np
+import pandas as pd
 import mne
-import matplotlib.pyplot as plt
 
 import torch.utils.data
 from torch.autograd import Variable
 
-from braindecode.datautil import create_from_X_y
+from braindecode.datasets.base import WindowsDataset
 
 from models.WGanGPSignalModels import Generator
 
@@ -18,10 +19,10 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 subject_id = 1
 
 # number of images to generate
-batch_size = 24
+batch_size = 36
 nimages = 128
 
-# gan info
+# GAN info
 sfreq = 250
 time_sample = 500
 noise = 100
@@ -30,7 +31,6 @@ cuda = True if torch.cuda.is_available() else False
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 
-tasks = ['feet', 'left_hand', 'right_hand', 'tongue']
 task_dict = {
     # Select just 'feet' task
     'feet': 0,
@@ -38,6 +38,7 @@ task_dict = {
     'right_hand': 2,
     'tongue': 3
 }
+tasks = ['feet', 'left_hand', 'right_hand', 'tongue']
 
 all_channels = ['Fz',
                 'FC3', 'FC1', 'FCz', 'FC2', 'FC4',
@@ -46,9 +47,10 @@ all_channels = ['Fz',
                 'P1', 'Pz', 'P2',
                 'POz']
 
-
+start = 0
+task_trials_epoch = []
 for task in tasks:
-    task_channels_X = np.empty(shape=(batch_size, 0, time_sample))
+    task_channels_trials = np.empty(shape=(batch_size, 0, time_sample))
 
     for channel in all_channels:
         # path to generator weights .pth file
@@ -73,52 +75,52 @@ for task in tasks:
 
         gen_sig = netG(z)
 
-        task_channels_X = np.append(task_channels_X, gen_sig.detach().cpu().numpy(), axis=1)
-
-    # task_channels_y = np.ones(shape=(batch_size, time_sample)) * target
+        task_channels_trials = np.append(task_channels_trials, gen_sig.detach().cpu().numpy(), axis=1)
 
     # ---------------------
     #  Merge channels
-    # ---------------------
-
-    # ---------------------
-    #  MNE Create Raw
+    #  MNE Create Task's Trial - Epoch
     # ---------------------
 
     # Creating Info objects
-    ch_types = ['eeg'] * 22
+    ch_types = ['eeg'] * len(all_channels)
     info = mne.create_info(all_channels, ch_types=ch_types, sfreq=sfreq)
     info.set_montage('standard_1020')
     info['description'] = 'My custom dataset'
 
-    # Creating Info objects
-    # data = np.array(task_channels_X[0])
-    #
-    # simulated_raw = mne.io.RawArray(data, info)
-    # simulated_raw.plot(show_scrollbars=False, show_scalebars=False)
-
     target = task_dict[task]
-    events = np.column_stack((np.arange(0, sfreq * batch_size, sfreq),
-                              np.ones(batch_size, dtype=int) * 1000,
-                              np.ones(shape=(batch_size), dtype=int) * target))
-
     event_dict = {task: target}
 
-    epoch_data = np.array(task_channels_X)
-    mne.EpochsArray(epoch_data, info)
-    simulated_epochs = mne.EpochsArray(epoch_data, info, tmin=-0.5, events=events,
-                                       event_id=event_dict)
-    simulated_epochs.plot(show_scrollbars=False, events=events,
-                          event_id=event_dict)
+    # Creating Epoch objects
+    for task_channels_trial in task_channels_trials:
 
-    print("end")
-    # windows_dataset = create_from_X_y(
-    #     X=task_channels_X,
-    #     y=task_channels_y,
-    #     drop_last_window=False,
-    #     sfreq=sfreq,
-    #     ch_names=all_channels,
-    #     window_stride_samples=500,
-    #     window_size_samples=500,
-    # )
-    # print(windows_dataset)
+        events = np.column_stack((np.arange(start, start + sfreq, sfreq),
+                                  np.ones(1, dtype=int) * 1000,
+                                  target))
+
+        metadata = pd.DataFrame({
+            'i_window_in_trial': np.arange(len(events)),
+            'i_start_in_trial': start + events[0][2],
+            'i_stop_in_trial': start + events[0][0] + time_sample,
+            'target': len(events) * [target]
+        })
+
+        epoch_data = np.array(task_channels_trial)
+        epoch_data = np.reshape(epoch_data, newshape=(-1, len(all_channels), 500))
+
+        simulated_epoch = mne.EpochsArray(epoch_data, info, tmin=-0.5, events=events, event_id=event_dict, metadata=metadata)
+        # simulated_epoch.plot(show_scrollbars=False, events=events, event_id=event_dict)
+        task_trials_epoch.append(simulated_epoch)
+        start += sfreq
+
+# ---------------------
+#  Merge Task
+#  Shuffle Tasks
+#  Create Fake Dataset - WindowsDataset
+# ---------------------
+
+random.shuffle(task_trials_epoch)
+session = mne.concatenate_epochs(task_trials_epoch)
+
+fake_dataset = WindowsDataset(session)
+
