cf3bc8a1ff1baabdbcea68ac7aa8387192f2f6bc
shiva <shiva_roshanravan@yahoo.com>
2021-08-21 04:05:58 +0430

    change dir of preprocesses

--- Code/Runs/PreProcess/bnci-preprocess.py
+++ Code/Runs/PreProcess/bnci-preprocess.py
@@ -6,6 +6,6 @@ subject_id = 1
 # dataset = load_data_moabb(dataset_name="BNCI2014001", subject_id=subject_id)
 # dataset = basic_preprocess(dataset, low_cut_hz=4., high_cut_hz=38., factor_new=1e-3, init_block_size=1000)
 
-dataset = load_preprocessed_data(data_path='../Data/Real_Data/BCI/', dataset_folder='bnci-raw/', subject_id=subject_id)
+dataset = load_preprocessed_data(data_path='../../../Data/Real_Data/BCI/', dataset_folder='bnci-raw/', subject_id=subject_id)
 dataset = reorder_based_hgd_channels(dataset)
-save_data(dataset, saving_path='../Data/Real_Data/BCI/bnci-raw/', subject_id=subject_id)
+save_data(dataset, saving_path='../../../Data/Real_Data/BCI/bnci-raw/', subject_id=subject_id)
