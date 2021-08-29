a7f2d3dd3508b33ffa885d31ece718cebf407369
shiva <shiva_roshanravan@yahoo.com>
2021-08-30 03:13:32 +0430

    .
    to revert changes

--- Code/Classification/TLClassification.py
+++ Code/Classification/TLClassification.py
@@ -27,7 +27,7 @@ def tl_classifier(train_set, valid_set,
                     dirname=save_path)
 
     # Early_stopping
-    early_stopping = EarlyStopping(patience=100)
+    early_stopping = EarlyStopping(patience=30)
 
     callbacks = [
         "accuracy",
@@ -39,8 +39,7 @@ def tl_classifier(train_set, valid_set,
     clf = EEGTLClassifier(
         model,
         double_channel=double_channel,
-        is_freezing=True,
-        warm_start=True,
+        # is_freezing=True,
         cropped=True,
         max_epochs=n_epochs,
         criterion=CroppedLoss,
--- Code/Runs/Cropped_Classification/run-bci-cropped.py
+++ Code/Runs/Cropped_Classification/run-bci-cropped.py
@@ -5,11 +5,11 @@ from Code.Classification.CroppedClassification import run_model
 
 subject_id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for subject_id in subject_id_list:
-    data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/' + str(subject_id_list).strip('[]')) + '/'
+    data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/' + str(subject_id)) + '/'
 
     # Path to saving Models
     # mkdir path to save
-    save_path = os.path.join('../../../Model_Params/BCI_Models/deep4/' + str(subject_id_list).strip('[]')) + '/Run-num: 0'
+    save_path = os.path.join('../../../Model_Params/BCI_Models/deep4/' + str(subject_id)) + '/Run-num: 1000/'
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
