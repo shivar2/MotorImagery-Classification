b1805e0e906f3947e7ec5d4d597327412939eb32
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-08-30 20:46:35 -0700

    Update run-cropped-fakeData.py
    windows can't mkdir file with ":"

--- Code/Runs/Fake_Cropped_Classification/run-cropped-fakeData.py
+++ Code/Runs/Fake_Cropped_Classification/run-cropped-fakeData.py
@@ -3,14 +3,13 @@ import os
 from Code.Classification.GanClassification import run_model
 
 # example of BNCI & fake data deep4 - subject 1
-subject_id_list = [1]
+subject_id_list = 1
 
 # Path to saving Models
 # mkdir path to save
-save_path = os.path.join('../../../Model_Params/Fake_Cropped_Classification/' + str(subject_id_list).strip('[]')) + '/Run: 1/'
-data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/' + str(subject_id_list).strip('[]')) + '/'
-fake_data_load_path = os.path.join('../../../Data/Fake_Data/WGan-GP-Signal/' + str(subject_id_list).strip('[]')) + '/Runs/'
-
+save_path = '../../../Model_Params/Fake_Cropped_Classification/' + str(subject_id_list) + '/Run 1/'
+data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/' + str(subject_id_list)) + '/'
+fake_data_load_path = os.path.join('../../../Data/Fake_Data/WGan-GP-Signal-VERSION2/' + str(subject_id_list)) + '/Runs/'
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
