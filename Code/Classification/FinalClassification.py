9ad13d937ec890e7db9643340b13843f20174655
shiva <shiva_roshanravan@yahoo.com>
2021-09-01 00:49:57 +0430

    Change Kfold in FinalClassification

--- Code/Classification/FinalClassification.py
+++ Code/Classification/FinalClassification.py
@@ -1,10 +1,8 @@
 import numpy as np
 import torch
 
-from sklearn.model_selection import train_test_split
-
 from skorch.callbacks import LRScheduler, Checkpoint, EarlyStopping
-from skorch.helper import predefined_split
+from skorch.dataset import CVSplit
 
 from braindecode.datautil.serialization import load_concat_dataset
 from braindecode.datasets.base import BaseConcatDataset
@@ -17,6 +15,8 @@ from braindecode.training.losses import CroppedLoss
 from Code.Classifier.EEGTLClassifier import EEGTLClassifier
 from Code.Models.PretrainedDeep4Model import PretrainedDeep4Model
 from Code.Classification.CroppedClassification import plot
+
+from Code.EarlyStopClass.EarlyStopClass import EarlyStopping
 
 
 def detect_device():
@@ -101,7 +101,7 @@ def split_data(windows_dataset):
     return train_set, valid_set
 
 
-def tl_classifier(train_set, valid_set,
+def tl_classifier(train_set,
                   save_path,
                   model,
                   double_channel=True,
@@ -137,14 +137,13 @@ def tl_classifier(train_set, valid_set,
     clf1 = EEGTLClassifier(
         model,
         double_channel=double_channel,
-        warm_start=True,
         is_freezing=True,
         cropped=True,
         max_epochs=n_epochs,
         criterion=CroppedLoss,
         criterion__loss_function=torch.nn.functional.nll_loss,
         optimizer=torch.optim.AdamW,
-        train_split=predefined_split(valid_set),
+        train_split=CVSplit(6),
         optimizer__lr=lr,
         optimizer__weight_decay=weight_decay,
         iterator_train__shuffle=True,
@@ -187,7 +186,7 @@ def tl_classifier(train_set, valid_set,
         criterion=CroppedLoss,
         criterion__loss_function=torch.nn.functional.nll_loss,
         optimizer=torch.optim.AdamW,
-        train_split=predefined_split(valid_set),
+        train_split=CVSplit(6),
         iterator_train__shuffle=True,
         batch_size=batch_size,
         callbacks=callbacks2,
@@ -197,8 +196,8 @@ def tl_classifier(train_set, valid_set,
     clf2.load_params(f_params=save_path + "params1.pt",
                      f_optimizer=save_path + "optimizers1.pt",
                      f_history=save_path + "history1.json")
-    phase2_train = BaseConcatDataset([train_set, valid_set])
-    clf2.fit(phase2_train, y=None)
+
+    clf2.fit(train_set, y=None)
     return clf2
 
 
@@ -238,18 +237,12 @@ def run_model(data_load_path, fake_data_load_path, double_channel, model_load_pa
                                           input_window_samples=input_window_samples,
                                           trial_start_offset_seconds=trial_start_offset_seconds)
 
-    train_set_all, test_set = split_data(windows_dataset)
-
-    # Split train_set to valid and train
-    X_train, X_valid = train_test_split(train_set_all.datasets, test_size=1, train_size=5)
-    train_set = BaseConcatDataset(X_train)
-    valid_set = BaseConcatDataset(X_valid)
+    train_set, test_set = split_data(windows_dataset)
 
     train_set_fake.append(train_set)
     X = BaseConcatDataset(train_set_fake)
 
     clf = tl_classifier(X,
-                        valid_set,
                         model=model,
                         save_path=save_path,
                         double_channel=double_channel,
