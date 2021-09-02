58e7757886352585019228c9cbfc92da04280f71
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-02 12:28:33 -0700

    Update hgd cropped on GPU

--- Code/Runs/Cropped_Classification/run-hgd-cropped.py
+++ Code/Runs/Cropped_Classification/run-hgd-cropped.py
@@ -5,11 +5,11 @@ from Code.Classification.HGDCroppedClassification import run_model
 
 # example of HGD deep4 - subject 1
 subject_id_list = [1]
-data_load_path = os.path.join('../../../Data/Real_Data/HGD/hgd-raw/' + str(subject_id_list).strip('[]')) + '/'
+data_load_path = os.path.join('../../../Data/Real_Data/HGD/hgd-22channels-raw/4-38/')
 
 # Path to saving Models
 # mkdir path to save
-save_path = os.path.join('../../../Model_Params/Pretrained_Models/deep4/' + str(subject_id_list).strip('[]')) + '/'
+save_path = os.path.join('../../../Model_Params/Pretrained_Models/deep4/22channel/4-38/')
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
