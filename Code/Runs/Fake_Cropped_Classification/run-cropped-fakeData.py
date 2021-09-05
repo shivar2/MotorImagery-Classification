e7fe9793be9a36d9a6dbc7213b31c37de54bdec5
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-04 18:38:33 -0700

    Update run-cropped-fakeData.py

--- Code/Runs/Fake_Cropped_Classification/run-cropped-fakeData.py
+++ Code/Runs/Fake_Cropped_Classification/run-cropped-fakeData.py
@@ -3,8 +3,8 @@ import os
 from Code.Classification.GanClassification import run_model
 
 # example of BNCI & fake data deep4 - subject 1
-subject_id_list = 1
-fake_k = 6
+subject_id_list = 3
+fake_k = 4
 
 # Path to saving Models
 # mkdir path to save
