1ba0396d01c6f40b8749f124cfaf490142f31aca
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-10 23:53:29 -0700

    edit dir - 44channels

--- Code/Runs/Final_Classification/run-bci-FinalClassification.py
+++ Code/Runs/Final_Classification/run-bci-FinalClassification.py
@@ -10,10 +10,10 @@ data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/4-38/' + str
 fake_data_load_path = os.path.join('../../../Data/Fake_Data/WGan-GP-Signal/' + str(subject_id_list).strip('[]')) + '/Runs/'
 
 # Load model path
-model_load_path = '../../../Model_Params/Pretrained_Models/with-resample/4-38/1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14/'
+model_load_path = '../../../Model_Params/Pretrained_Models/without-resample/4-38/1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14/'
 
 # Save results
-save_path = os.path.join('../../../Model_Params/Final_Classification/with-resample/4-38/' + str(subject_id_list).strip('[]')) + '/Run 1/'
+save_path = os.path.join('../../../Model_Params/Final_Classification/without-resample/4-38/' + str(subject_id_list).strip('[]')) + '/Run 1/'
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
--- Code/Runs/TL_Classification/run-bci-tl-classification.py
+++ Code/Runs/TL_Classification/run-bci-tl-classification.py
@@ -8,15 +8,15 @@ for subject_id in subject_id_list:
     data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/4-38/' + str(subject_id)) + '/'
 
     # Load model path
-    model_load_path = '../../../Model_Params/Pretrained_Models/with-resample/4-38/'
+    model_load_path = '../../../Model_Params/Pretrained_Models/44channels/without-resample/4-38/'
 
     # Save results
-    save_path = os.path.join('../../../Model_Params/TL_Classification/with-resample/4-38/' + str(subject_id)) + '/Run 0/'
+    save_path = os.path.join('../../../Model_Params/TL_Classification/44channels/without-resample/4-38/' + str(subject_id)) + '/Run 0/'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     run_model(data_load_path=data_load_path,
-              double_channel=False,
+              double_channel=True,
               model_load_path=model_load_path,
-              params_name='params_10.pt',
+              params_name='params_16.pt',
               save_path=save_path)
