3a68afe53df22fe8e7577010bed97eaa38ec6b0d
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-14 04:16:31 -0700

    tL dir

--- Code/Runs/TL_Classification/run-bci-tl-classification-phase2.py
+++ Code/Runs/TL_Classification/run-bci-tl-classification-phase2.py
@@ -11,12 +11,13 @@ for subject_id in subject_id_list:
     model_load_path = '../../../Model_Params/Pretrained_Models/22channels/0-f/'
 
     # Save results
-    save_path = os.path.join('../../../Model_Params/TL_Classification/phase1/22channels/0-38/' + str(subject_id)) + '/Run 1/'
+    save_path = os.path.join('../../../Model_Params/TL_Classification/phase1/22channels/0-38/'
+                             + 'Run params_19/' + str(subject_id)) + '/run 1/'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     run_model(data_load_path=data_load_path,
               double_channel=False,
               model_load_path=model_load_path,
-              params_name='params_16.pt',
+              params_name='params_19.pt',
               save_path=save_path)
