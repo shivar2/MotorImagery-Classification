f6de14b8c176100e01bb8f4599304fb5fed0024d
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-16 07:41:05 -0700

    Update test-cropped-real-fake-phase2.py

--- Code/Tests/Phase2/test-cropped-real-fake-phase2.py
+++ Code/Tests/Phase2/test-cropped-real-fake-phase2.py
@@ -97,17 +97,17 @@ def test_clf(data_load_path, clf_load_path, save_path):
 #   Test Cropped And Fake Classification
 ########################################
 
-subject_id_list = [1]
+subject_id_list = [9]
 for subject_id in subject_id_list:
     data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/' + str(subject_id)) + '/'
 
-    clf_load_path = os.path.join('../../../Model_Params/BCI_Models/phase2/0-38/' + str(subject_id)) + '/Run 1/'
-    # clf_load_path = os.path.join('../../../Model_Params/Fake_Cropped_Classification/phase2/0-38/' +
-    #                              str(subject_id)) + '/Run 1/'
+    # clf_load_path = os.path.join('../../../Model_Params/BCI_Models/phase2/0-38/' + str(subject_id)) + '/Run 1/'
+    clf_load_path = os.path.join('../../../Model_Params/Fake_Cropped_Classification/phase2/0-38/' +
+                                 str(subject_id)) + '/Run 2/'
 
-    save_path = os.path.join('../../../Result/BCI_Models/phase2/0-38/' + str(subject_id)) + '/Run 1/'
-    # save_path = os.path.join('../../../Result/Fake_Cropped_Classification/phase2/0-38/' +
-    #                          str(subject_id)) + '/Run 1/'
+    # save_path = os.path.join('../../../Result/BCI_Models/phase2/0-38/' + str(subject_id)) + '/Run 1/'
+    save_path = os.path.join('../../../Result/Fake_Cropped_Classification-MAX/phase2/0-38/' +
+                             str(subject_id)) + '/Run 2/'
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
