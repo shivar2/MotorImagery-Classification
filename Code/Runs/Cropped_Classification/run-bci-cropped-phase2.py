3206bd0ce56bde9a7fb48fce37829819b7d3e2f8
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-16 03:33:02 -0700

    Update run-bci-cropped-phase2.py

--- Code/Runs/Cropped_Classification/run-bci-cropped-phase2.py
+++ Code/Runs/Cropped_Classification/run-bci-cropped-phase2.py
@@ -5,11 +5,11 @@ from Code.ClassificationPHASE2.CroppedClassification2phase import run_model
 
 subject_id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for subject_id in subject_id_list:
-    data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/4-38/' + str(subject_id)) + '/'
+    data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/' + str(subject_id)) + '/'
 
     # Path to saving Models
     # mkdir path to save
-    save_path = os.path.join('../../../Model_Params/BCI_Models/phase2/4-38/' + str(subject_id)) + '/Run 1/'
+    save_path = os.path.join('../../../Model_Params/BCI_Models/phase2/0-38/' + str(subject_id)) + '/Run 1/'
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
