48447c22644cce079f1ef248df090ecc5c225aa1
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-16 13:17:25 -0700

    HGD Classification 2phase

--- Code/Classification/HGDCroppedClassification.py
+++ Code/Classification/HGDCroppedClassification.py
@@ -1,35 +1,22 @@
 import numpy as np
 import torch
 
-from skorch.callbacks import LRScheduler, Checkpoint
-from skorch.helper import predefined_split
 from torch.utils.data import Subset
 
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-import pandas as pd
+from skorch.callbacks import LRScheduler, Checkpoint
+from skorch.helper import predefined_split
 
 from braindecode.datautil.serialization import load_concat_dataset
 from braindecode.datasets.base import BaseConcatDataset
 from braindecode.util import set_random_seeds
-from braindecode.models import ShallowFBCSPNet, Deep4Net
 from braindecode.models.util import to_dense_prediction_model, get_output_shape
-from braindecode.datautil.windowers import create_windows_from_events
 from braindecode import EEGClassifier
 from braindecode.training.losses import CroppedLoss
 
-
-from braindecode.datautil.preprocess import preprocess, Preprocessor
-from Code.Preprocess import MaxNormalize
-
-
-def detect_device():
-    cuda = torch.cuda.is_available()  # check if GPU is available, if True chooses to use it
-    device = 'cuda' if cuda else 'cpu'
-    if cuda:
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-    return cuda, device
+from Code.EarlyStopClass.EarlyStopClass import EarlyStopping
+from Code.base import detect_device,\
+    create_model_deep4, create_model_shallow, cut_compute_windows,\
+    get_test_data, plot
 
 
 def load_data_object(data_path):
@@ -49,52 +36,6 @@ def load_data_object(data_path):
     return dataset_obj
 
 
-def create_model_shallow(input_window_samples=1000, n_chans=4, n_classes=4):
-    model = ShallowFBCSPNet(
-        n_chans,
-        n_classes,
-        input_window_samples=input_window_samples,
-        final_conv_length=30,
-    )
-    return model
-
-
-def create_model_deep4(input_window_samples=1000, n_chans=4, n_classes=4):
-    model = Deep4Net(
-        in_chans=n_chans,
-        n_classes=n_classes,
-        input_window_samples=input_window_samples,
-        final_conv_length=2,
-    )
-    return model
-
-
-def cut_compute_windows(dataset, n_preds_per_input, input_window_samples=1000, trial_start_offset_seconds=-0.5):
-    # Extract sampling frequency, check that they are same in all datasets
-    sfreq = dataset.datasets[0].raw.info['sfreq']
-    assert all([ds.raw.info['sfreq'] == sfreq for ds in dataset.datasets])
-
-    # Calculate the trial start offset in samples.
-    trial_start_offset_samples = int(trial_start_offset_seconds * sfreq)
-
-    # Create windows using braindecode function for this. It needs parameters to define how
-    # trials should be used.
-    windows_dataset = create_windows_from_events(
-        dataset,
-        trial_start_offset_samples=trial_start_offset_samples,
-        trial_stop_offset_samples=0,
-        window_size_samples=input_window_samples,
-        window_stride_samples=n_preds_per_input,
-        drop_last_window=False,
-        preload=False,
-        mapping={'left_hand': 0, 'right_hand': 1, 'feet': 2, 'rest': 3},
-    )
-    # max normalize
-    preprocess(windows_dataset, [Preprocessor(MaxNormalize)])
-
-    return windows_dataset
-
-
 def split_into_train_valid(windows_dataset, use_final_eval):
     splitted = windows_dataset.split('run')
     if use_final_eval:
@@ -111,32 +52,30 @@ def split_into_train_valid(windows_dataset, use_final_eval):
     return train_set, valid_set
 
 
-def train_cropped_trials(train_set, valid_set, model, save_path, model_name='shallow', device='cpu'):
-    if model_name == 'shallow':
-        # These values we found good for shallow network:
-        lr = 0.0625 * 0.01
-        weight_decay = 0
-    else:
-        # For deep4 they should be:
-        lr = 1 * 0.01
-        weight_decay = 0.5 * 0.001
+def train_cropped_trials(train_set_all, model, save_path, device='cpu'):
+
+    train_set, valid_set = split_into_train_valid(train_set_all, use_final_eval=False)
 
     batch_size = 64
-    n_epochs = 35
+    n_epochs = 800
 
     # Checkpoint will save the model with the lowest valid_loss
-    cp = Checkpoint(monitor=None,
-                    f_params="params_{last_epoch[epoch]}.pt",
-                    f_optimizer="optimizer_{last_epoch[epoch]}.pt",
-                    f_history="history.json",
+    cp = Checkpoint(monitor='valid_accuracy_best',
+                    f_params="params1.pt",
+                    f_optimizer="optimizers1.pt",
+                    f_history="history1.json",
                     dirname=save_path, f_criterion=None)
+
+    # Early_stopping
+    early_stopping = EarlyStopping(monitor='valid_accuracy', lower_is_better=False, patience=80)
 
     callbacks = [
         "accuracy",
         ('cp', cp),
-        ("lr_scheduler", LRScheduler('CosineAnnealingLR', T_max=n_epochs - 1)),
+        ('patience', early_stopping),
     ]
-    clf = EEGClassifier(
+
+    clf1 = EEGClassifier(
         model,
         cropped=True,
         max_epochs=n_epochs,
@@ -144,8 +83,6 @@ def train_cropped_trials(train_set, valid_set, model, save_path, model_name='sha
         criterion__loss_function=torch.nn.functional.nll_loss,
         optimizer=torch.optim.AdamW,
         train_split=predefined_split(valid_set),
-        optimizer__lr=lr,
-        optimizer__weight_decay=weight_decay,
         iterator_train__shuffle=True,
         batch_size=batch_size,
         callbacks=callbacks,
@@ -153,61 +90,65 @@ def train_cropped_trials(train_set, valid_set, model, save_path, model_name='sha
     )
     # Model training for a specified number of epochs. `y` is None as it is already supplied
     # in the dataset.
-    clf.fit(train_set, y=None)
-    return clf
+    clf1.fit(train_set, y=None)
 
+    # PHASE 2
+    # Best clf1 valid accuracy
+    best_valid_acc_epoch = np.argmax(clf1.history[:, 'valid_accuracy'])
+    target_train_loss = clf1.history[best_valid_acc_epoch, 'train_loss']
 
-def plot(clf, save_path):
-    # Extract loss and accuracy values for plotting from history object
-    results_columns = ['train_loss', 'valid_loss', 'train_accuracy', 'valid_accuracy']
-    df = pd.DataFrame(clf.history[:, results_columns], columns=results_columns,
-                      index=clf.history[:, 'epoch'])
+    # Early_stopping
+    early_stopping2 = EarlyStopping(monitor='valid_loss',
+                                    divergence_threshold=target_train_loss,
+                                    patience=80)
 
-    # get percent of misclass for better visual comparison to loss
-    df = df.assign(train_misclass=100 - 100 * df.train_accuracy,
-                   valid_misclass=100 - 100 * df.valid_accuracy)
+    # Checkpoint will save the model with the lowest valid_loss
+    cp2 = Checkpoint(
+                     f_params="params2.pt",
+                     f_optimizer="optimizers2.pt",
+                     dirname=save_path,
+                     f_criterion=None)
 
-    plt.style.use('seaborn')
-    fig, ax1 = plt.subplots(figsize=(8, 3))
-    df.loc[:, ['train_loss', 'valid_loss']].plot(
-        ax=ax1, style=['-', ':'], marker='o', color='tab:blue', legend=False, fontsize=14)
+    callbacks2 = [
+        "accuracy",
+        ('cp', cp2),
+        ('patience', early_stopping2),
+    ]
 
-    ax1.tick_params(axis='y', labelcolor='tab:blue', labelsize=14)
-    ax1.set_ylabel("Loss", color='tab:blue', fontsize=14)
+    clf2 = EEGClassifier(
+        model,
+        cropped=True,
+        warm_start=True,
+        max_epochs=n_epochs,
+        criterion=CroppedLoss,
+        criterion__loss_function=torch.nn.functional.nll_loss,
+        optimizer=torch.optim.AdamW,
+        train_split=predefined_split(valid_set),
+        iterator_train__shuffle=True,
+        batch_size=batch_size,
+        callbacks=callbacks2,
+        device=device,
+    )
 
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    clf2.initialize()  # This is important!
+    clf2.load_params(f_params=save_path+"params1.pt",
+                     f_optimizer=save_path+"optimizers1.pt",
+                     f_history=save_path+"history1.json")
 
-    df.loc[:, ['train_misclass', 'valid_misclass']].plot(
-        ax=ax2, style=['-', ':'], marker='o', color='tab:red', legend=False)
-    ax2.tick_params(axis='y', labelcolor='tab:red', labelsize=14)
-    ax2.set_ylabel("Misclassification Rate [%]", color='tab:red', fontsize=14)
-    ax2.set_ylim(ax2.get_ylim()[0], 85)  # make some room for legend
-    ax1.set_xlabel("Epoch", fontsize=14)
-
-    # where some data has already been plotted to ax
-    handles = []
-    handles.append(Line2D([0], [0], color='black', linewidth=1, linestyle='-', label='Train'))
-    handles.append(Line2D([0], [0], color='black', linewidth=1, linestyle=':', label='Valid'))
-    plt.legend(handles, [h.get_label() for h in handles], fontsize=14)
-    plt.tight_layout()
-
-    # Image path
-    image_path = save_path + 'result'
-    plt.savefig(fname=image_path)
+    clf2.fit(train_set_all, y=None)
+    return clf2
 
 
 def run_model(data_load_path, model_name, save_path):
     input_window_samples = 1000
     cuda, device = detect_device()
 
-    seed = 20200220  # random seed to make results reproducible
-    # Set random seed to be able to reproduce results
+    seed = 20200220
     set_random_seeds(seed=seed, cuda=cuda)
 
     dataset = load_data_object(data_load_path)
 
     n_classes = 4
-    # Extract number of chans and time steps from dataset
     n_chans = dataset[0][0].shape[0]
 
     if model_name == 'shallow':
@@ -223,7 +164,6 @@ def run_model(data_load_path, model_name, save_path):
     # so we can use it to obtain predictions for all crops.
     to_dense_prediction_model(model)
 
-    # To know the Models’ receptive field, we calculate the shape of model output for a dummy input.
     n_preds_per_input = get_output_shape(model, n_chans, input_window_samples)[2]
 
     trial_start_offset_seconds = -0.5
@@ -233,15 +173,48 @@ def run_model(data_load_path, model_name, save_path):
                                           input_window_samples=input_window_samples,
                                           trial_start_offset_seconds=trial_start_offset_seconds)
 
-    train_set, valid_set = split_into_train_valid(windows_dataset, use_final_eval=True)
+    train_set_all, test_set = split_into_train_valid(windows_dataset, use_final_eval=True)
 
-    clf = train_cropped_trials(train_set,
-                               valid_set,
+    clf = train_cropped_trials(train_set_all,
                                model=model,
                                save_path=save_path,
-                               model_name=model_name,
                                device=device)
 
     plot(clf, save_path)
+
+    # Load best Classifier and model for Test
+    clf_best = EEGClassifier(
+        model,
+        cropped=True,
+        max_epochs=1,
+        criterion=CroppedLoss,
+        criterion__loss_function=torch.nn.functional.nll_loss,
+        optimizer=torch.optim.AdamW,
+        iterator_train__shuffle=True,
+        batch_size=64,
+        device=device,
+    )
+
+    clf_best.initialize()  # This is important!
+    clf_best.load_params(f_params=save_path + "params2.pt",
+                         f_optimizer=save_path + "optimizers2.pt",
+                         f_history=save_path + "history.json")
+    # Calculate Mean Accuracy For Test set
+    i = 0
+    test = np.empty(shape=(len(test_set), n_chans, input_window_samples))
+    target = np.empty(shape=(len(test_set)))
+    for x, y, window_ind in test_set:
+        test[i] = x
+        target[i] = y
+        i += 1
+
+    score = clf_best.score(test, y=target)
+    print("EEG Cropped Classification Score (Accuracy) is:  " + str(score))
+
+    f = open(save_path + "test-result.txt", "w")
+    f.write("EEG Cropped Classification Score (Accuracy) is:  " + str(score))
+    f.close()
+
+
 
 
