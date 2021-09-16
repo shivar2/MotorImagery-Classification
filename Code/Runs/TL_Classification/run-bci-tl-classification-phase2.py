e5ecadc03a256be9ad587c95ca555091e574c75a
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-16 14:15:38 -0700

    Update run-bci-tl-classification-phase2.py

--- Code/Runs/TL_Classification/run-bci-tl-classification-phase2.py
+++ Code/Runs/TL_Classification/run-bci-tl-classification-phase2.py
@@ -3,7 +3,7 @@ import os
 from Code.ClassificationPHASE2.TLClassification2phase import run_model
 
 
-subject_id_list = [1]
+subject_id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for subject_id in subject_id_list:
     data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/' + str(subject_id)) + '/'
 
@@ -12,12 +12,12 @@ for subject_id in subject_id_list:
 
     # Save results
     save_path = os.path.join('../../../Model_Params/TL_Classification/phase1/22channels/0-38/'
-                             + 'Run params_19/' + str(subject_id)) + '/run 1/'
+                             + str(subject_id)) + '/run 1/'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     run_model(data_load_path=data_load_path,
               double_channel=False,
               model_load_path=model_load_path,
-              params_name='params_19.pt',
+              params_name='params2.pt',
               save_path=save_path)
