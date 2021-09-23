1e839ac8ebf17a54f02c2c7306ec08197c2d0b28
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-22 19:26:17 -0700

    Update bnci-preprocess.py

--- Code/Runs/PreProcess/bnci-preprocess.py
+++ Code/Runs/PreProcess/bnci-preprocess.py
@@ -5,7 +5,7 @@ for subject_id in range(1, 10):
 
     # dataset = load_data_moabb(dataset_name="BNCI2014001", subject_id=subject_id)
     # dataset = basic_preprocess(dataset, low_cut_hz=None, high_cut_hz=38., factor_new=1e-3, init_block_size=1000)
-    dataset = load_preprocessed_data(data_path='../../../Data/Real_Data/BCI/bnci-raw/0-38/reorder channel/',
+    dataset = load_preprocessed_data(data_path='../../../Data/Real_Data/BCI/bnci-raw/0-38/',
                                      subject_id=subject_id)
 
     dataset = add_channel_to_raw(dataset)
