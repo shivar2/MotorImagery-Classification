28ca572d9bf5538085a86985c293f01ce5a9baa0
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-23 18:07:29 -0700

    Update run-hgd-cropped.py

--- Code/Runs/Cropped_Classification/run-hgd-cropped.py
+++ Code/Runs/Cropped_Classification/run-hgd-cropped.py
@@ -9,7 +9,7 @@ data_load_path = os.path.join('../../../Data/Real_Data/HGD/22channels/0-f/')
 # Path to saving Models
 # mkdir path to save
 # save_path = os.path.join('../../../Model_Params/Pretrained_Models/42channels/0-f/')
-save_path = os.path.join('../../../Model_Params/Pretrained_Models/42channels/0-f/')
+save_path = os.path.join('../../../Model_Params/HGD_Models/42channels/0-f/')
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
