9e61e07b23a83612917ef592ef155caa56289861
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-23 18:07:04 -0700

    Edit hgd classification for run per subject

--- Code/Runs/Cropped_Classification/run-hgd-cropped.py
+++ Code/Runs/Cropped_Classification/run-hgd-cropped.py
@@ -4,15 +4,17 @@ from Code.Classification.HGDCroppedClassification import run_model
 
 
 # example of HGD deep4 - subject 1
-subject_id_list = [1]
+subject_id = 1
 data_load_path = os.path.join('../../../Data/Real_Data/HGD/22channels/0-f/')
 # Path to saving Models
 # mkdir path to save
-save_path = os.path.join('../../../Model_Params/Pretrained_Models/22channels/0-f/')
+# save_path = os.path.join('../../../Model_Params/Pretrained_Models/42channels/0-f/')
+save_path = os.path.join('../../../Model_Params/Pretrained_Models/42channels/0-f/')
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
 run_model(data_load_path=data_load_path,
+          subject_id=subject_id,
           model_name='deep4',
           save_path=save_path)
