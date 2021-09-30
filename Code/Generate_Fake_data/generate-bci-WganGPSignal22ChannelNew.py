0818406a73ebeb42fa716adfca2b170d22f2564d
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-09-30 03:23:33 -0700

    Add WGan - 22channels VERSION7

---
+++ Code/Generate_Fake_data/generate-bci-WganGPSignal22ChannelNew.py
@@ -0,0 +1,128 @@
+
+import os
+import random
+import numpy as np
+import pandas as pd
+import mne
+
+import torch.utils.data
+from torch.autograd import Variable
+
+from braindecode.datasets.base import WindowsDataset, BaseConcatDataset
+
+from Code.Models.GANs.WGanGPSignalModels import Generator
+
+
+subject_id_list = [8]
+normalizer_name = 'MaxNormalized'       # 'tanhNormalized/'
+
+# number of images to generate
+batch_size = 24 * 4
+
+# GAN info
+sfreq = 250
+time_sample = 250
+noise = 100
+
+cuda = True if torch.cuda.is_available() else False
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+# mapping to HGD tasks
+task_dict = {'left_hand': 0, 'right_hand': 1, 'feet': 2, 'tongue': 3}
+tasks = ['feet', 'left_hand', 'right_hand', 'tongue']
+
+all_channels = [
+        'FC1', 'FC2',
+        'C3', 'Cz', 'C4',
+        'CP1', 'CP2',
+        'FC3', 'FCz', 'FC4', 'C5', 'C1', 'C2', 'C6',
+        'CP3', 'CPz', 'CP4',
+        'Fz', 'P1', 'Pz', 'P2', 'POz']
+
+for subject_id in subject_id_list:
+
+    for run in range(0, 10):
+        start = 0
+        task_trials_epoch = []
+
+        for task in tasks:
+
+            # path to generator weights .pth file
+            saved_models_path = '../../Model_Params/GANs/WGan-GP-Signal-VERSION7/' +\
+                                    normalizer_name + str(subject_id) + '/' + task + '/'
+            saved_models_path += 'generator_state_dict.pth'
+
+            netG = Generator(time_sample=time_sample, noise=noise, channels=22)
+
+            # load weights
+            netG.load_state_dict(torch.load(saved_models_path))
+
+            if cuda:
+                netG.cuda()
+
+            # initialize noise
+            z = Variable(Tensor(np.random.normal(0, 1, (batch_size, noise))))
+
+            gen_sig = netG(z)
+
+            task_channels_trial = gen_sig.detach().cpu().numpy()
+
+            # ---------------------
+            #  Merge channels
+            #  MNE Create Task's Trial - Epoch
+            # ---------------------
+
+            # Creating Info objects
+            ch_types = ['eeg'] * len(all_channels)
+            info = mne.create_info(all_channels, ch_types=ch_types, sfreq=sfreq)
+            info.set_montage('standard_1020')
+            info['description'] = 'My custom dataset'
+
+            target = task_dict[task]
+            event_dict = {task: target}
+
+            # merge 4s together to create 1 epoch
+            task_channels_trial = task_channels_trial.reshape(-1, 22, 1000)
+            # Creating Epoch objects
+
+            for tct in task_channels_trial:
+                    events = np.column_stack((np.arange(start, start + int(sfreq/2), sfreq),
+                                              np.ones(1, dtype=int) * 1000,
+                                              target))
+
+                    metadata = pd.DataFrame({
+                        'i_window_in_trial': np.arange(len(events)),
+                        'i_start_in_trial': start + events[0][2],
+                        'i_stop_in_trial': start + events[0][0] + time_sample,
+                        'target': len(events) * [target]
+                    })
+
+                    epoch_data = np.array(tct)
+                    epoch_data = np.reshape(epoch_data, newshape=(-1, len(all_channels), time_sample*4))
+
+                    simulated_epoch = mne.EpochsArray(epoch_data, info, tmin=-0.5, events=events, event_id=event_dict, metadata=metadata)
+                    # simulated_epoch.plot(show_scrollbars=False, events=events, event_id=event_dict)
+                    task_trials_epoch.append(simulated_epoch)
+                    start += sfreq
+
+        # ---------------------
+        #  Merge Task
+        #  Shuffle Tasks
+        #  Create Fake Dataset - WindowsDataset
+        # ---------------------
+
+        random.shuffle(task_trials_epoch)
+        session = mne.concatenate_epochs(task_trials_epoch)
+
+        # Save fake dataset as BaseConcatDataset obj
+        wdataset = WindowsDataset(session)
+        fake_dataset = BaseConcatDataset([wdataset])
+
+        # path to to fake eeg directory
+        fake_data_path = '../../Data/Fake_Data/WGan-GP-Signal-VERSION7/' + str(subject_id) + '/' + 'Runs' + '/' + str(run) +'/'
+        if not os.path.exists(fake_data_path):
+            os.makedirs(fake_data_path)
+
+        fake_dataset.save(path=fake_data_path, overwrite=True)
+
+
---
+++ Code/Runs/GANs/run-wganGP-erik-signal-22channels.py
@@ -0,0 +1,133 @@
+"""
+main base code:
+    https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/wgan_gp/wgan_gp.py
+"""
+import os
+import torch
+import numpy as np
+
+from braindecode.datautil.windowers import create_windows_from_events
+from braindecode.datautil.serialization import load_concat_dataset
+from braindecode.util import set_random_seeds
+
+from Code.Models.GANs.WGanGPErikSignal import WGANGP
+
+
+def get_data(data_load_path,
+             time_sample=32,
+             window_stride_samples=1,
+             mapping=None,
+             pick_channels=None):
+    # Dataset
+    dataset = load_concat_dataset(
+        path=data_load_path,
+        preload=False,
+        target_name=None,
+    )
+
+    sfreq = dataset.datasets[0].raw.info['sfreq']
+    assert all([ds.raw.info['sfreq'] == sfreq for ds in dataset.datasets])
+
+    trial_start_offset_samples = int(-0.5 * sfreq)
+
+    windows_dataset = create_windows_from_events(
+        dataset,
+        trial_start_offset_samples=trial_start_offset_samples,
+        trial_stop_offset_samples=0,
+        preload=True,
+        window_size_samples=time_sample,
+        window_stride_samples=window_stride_samples,
+        drop_bad_windows=True,
+        picks=pick_channels,
+        mapping=mapping,
+    )
+
+    splitted = windows_dataset.split('session')
+    train_set = splitted['session_T']
+    n_chans = windows_dataset[0][0].shape[0]
+
+    i = 0
+    events_num = train_set.datasets[0].windows.events.shape[0]
+    runs_num = len(train_set.datasets)
+    epochs_num = events_num * runs_num
+
+    data = np.empty(shape=(epochs_num, n_chans, time_sample))
+    for x, y, window_ind in train_set:
+        data[i] = x
+        i += 1
+    data = data.reshape(-1, n_chans, 250)
+
+    # max normalize
+    max = np.max(data, keepdims=True, axis=-1)
+    normal_data = data / max
+
+    return normal_data, n_chans
+
+
+#########################
+# load data             #
+#########################
+subject_id = 8
+data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/22channels/' + str(subject_id)) + '/'
+
+normalizer_name = 'MaxNormalized/'
+
+time_sample = 1000
+window_stride_samples = 467
+mapping = {'left_hand': 0, 'right_hand': 1, 'feet': 2, 'tongue': 3 }
+
+all_channels = ['Fz',
+                 'FC1', 'FC2',
+                 'C3', 'Cz', 'C4', 'CP1', 'CP2',
+                 'Pz', 'POz', 'FC3', 'FCz', 'FC4',
+                 'C5', 'C1', 'C2', 'C6', 'CP3', 'CPz', 'CP4',
+                 'P1', 'P2']
+
+for key, value in mapping.items():
+        tasks_name = key
+        task_mapping = {
+            key: value
+        }
+
+        save_result_path = '../../../Result/GANs/WGan-GP-Signal-VERSION7/' + normalizer_name + str(
+            subject_id) + '/' + tasks_name + '/'
+        if not os.path.exists(save_result_path):
+            os.makedirs(save_result_path)
+
+        save_model_path = '../../../Model_Params/GANs/WGan-GP-Signal-VERSION7/' + normalizer_name + str(
+            subject_id) + '/' + tasks_name + '/'
+        if not os.path.exists(save_model_path):
+            os.makedirs(save_model_path)
+
+        cuda = True if torch.cuda.is_available() else False
+
+        seed = 20200220  # random seed to make results reproducible
+        set_random_seeds(seed=seed, cuda=cuda)
+
+        data, n_chans = get_data(data_load_path=data_load_path,
+                                 time_sample=time_sample,
+                                 window_stride_samples=window_stride_samples,
+                                 mapping=task_mapping,
+                                 pick_channels=all_channels
+                                 )
+
+        #########################
+        # Running params        #
+        #########################
+
+        batchsize = 64
+        epochs = 350
+
+        net = WGANGP(subject=subject_id,
+                     n_epochs=epochs,
+                     batch_size=batchsize,
+                     time_sample=250,
+                     channels=n_chans,
+                     sample_interval=400,
+                     result_path=save_result_path,
+                     )
+
+        net.train(data, save_model_path=save_model_path)
+
+print("end")
+
