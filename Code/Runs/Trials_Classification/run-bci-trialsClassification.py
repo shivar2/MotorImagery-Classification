840731b7153d375f5667aa454f6517e8cc44883e
shiva <shiva_roshanravan@yahoo.com>
2021-10-01 07:32:02 +0330

    Add New Trial classification

--- Code/Runs/Trials_Classification/run-bci-trialsClassification.py
+++ Code/Runs/Trials_Classification/run-bci-trialsClassification.py
@@ -1,22 +1,63 @@
 import os
 
-from Code.Classification.TrialsClassification import run_model
+from braindecode.util import set_random_seeds
 
+from Code.base import load_data_object, create_model_deep4,\
+    create_model_newDeep4, create_model_newDeep4_3d, detect_device
 
-# example of BNCI shallow - subject 1
-subject_id_list = [1]
-data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/' + str(subject_id_list).strip('[]')) + '/'
+from Code.TrialClassifications import TrialsClassification
 
-# Save result
-save_path = os.path.join('../../../Result/Trials_Classification/BCI/' + str(subject_id_list).strip('[]')) + '/'
+# Run Info
+subject_id_list = [8]
+phase_number = '2'
+model_name = "deep4"
+normalize = True
 
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
+if normalize:
+    normalize_str = 'normalize/'
+else:
+    normalize_str = 'notNormalize/'
 
-print('BCI  -  Trials  -  Shallow  -  Subject ' + str(subject_id_list).strip('[]'))
+cuda, device = detect_device()
+seed = 20200220
+set_random_seeds(seed=seed, cuda=cuda)
 
-run_model(data_load_path=data_load_path,
-          dataset_name='BNCI',
-          model_name='shallow',
-          save_path=save_path)
+for subject_id in subject_id_list:
+    # data
+    if model_name == 'deep4':
+        if normalize:
+            data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/22channels-zmax/' + str(subject_id)) + '/'
+        else:
+            data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/22channels/' + str(subject_id)) + '/'
+    else:
+        data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/42channels/' + str(subject_id)) + '/'
+
+    dataset = load_data_object(data_load_path)
+
+    input_window_samples = 1000
+    n_classes = 4
+    n_chans = dataset[0][0].shape[0]
+
+    if model_name == 'deep4':
+        model = create_model_deep4(input_window_samples, n_chans, n_classes)
+
+    elif model_name == 'deep4New':
+        model = create_model_newDeep4(input_window_samples, n_chans, n_classes)
+
+    else:
+        model = create_model_newDeep4_3d(input_window_samples, n_chans, n_classes)
+
+    # Send model to GPU
+    if cuda:
+        model.cuda()
+
+    # Path to saving Models
+    # mkdir path to save
+    save_path = os.path.join('../../../Model_Params/BCI_Models_Trials/0-38/' +
+                             model_name + '/' + phase_number + ' - ' + normalize_str + str(subject_id)) + '/'
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    TrialsClassification.run_model(dataset=dataset, model=model, phase=phase_number, save_path=save_path)
 
