92d45b10988146d24e666dd9546bbb52c1d341b4
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-10-01 13:51:34 -0700

    Update run-wganGP-erik-signal-22channels.py
    for run all subjects

--- Code/Runs/GANs/run-wganGP-erik-signal-22channels.py
+++ Code/Runs/GANs/run-wganGP-erik-signal-22channels.py
@@ -68,64 +68,65 @@ cuda = True if torch.cuda.is_available() else False
 seed = 20200220  # random seed to make results reproducible
 set_random_seeds(seed=seed, cuda=cuda)
 
-#########################
-# Load data            #
-#########################
-subject_id = 8
-data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/22channels-zmax/' + str(subject_id)) + '/'
+for subject_id in range(1, 10):
+    #########################
+    # Load data            #
+    #########################
 
-mapping = {'left_hand': 0, 'right_hand': 1, 'feet': 2, 'tongue': 3 }
+    data_load_path = os.path.join('../../../Data/Real_Data/BCI/bnci-raw/0-38/22channels-zmax/' + str(subject_id)) + '/'
 
-all_channels = ['Fz',
-                 'FC1', 'FC2',
-                 'C3', 'Cz', 'C4', 'CP1', 'CP2',
-                 'Pz', 'POz', 'FC3', 'FCz', 'FC4',
-                 'C5', 'C1', 'C2', 'C6', 'CP3', 'CPz', 'CP4',
-                 'P1', 'P2']
+    mapping = {'left_hand': 0, 'right_hand': 1, 'feet': 2, 'tongue': 3 }
 
-time_sample = 1000
-window_stride_samples = 467
+    all_channels = ['Fz',
+                     'FC1', 'FC2',
+                     'C3', 'Cz', 'C4', 'CP1', 'CP2',
+                     'Pz', 'POz', 'FC3', 'FCz', 'FC4',
+                     'C5', 'C1', 'C2', 'C6', 'CP3', 'CPz', 'CP4',
+                     'P1', 'P2']
 
-for key, value in mapping.items():
-        tasks_name = key
-        task_mapping = {
-            key: value
-        }
+    time_sample = 1000
+    window_stride_samples = 467
 
-        save_result_path = '../../../Result/GANs/WGan-GP-Signal-VERSION7/' + str(
-            subject_id) + '/' + tasks_name + '/'
-        if not os.path.exists(save_result_path):
-            os.makedirs(save_result_path)
+    for key, value in mapping.items():
+            tasks_name = key
+            task_mapping = {
+                key: value
+            }
 
-        save_model_path = '../../../Model_Params/GANs/WGan-GP-Signal-VERSION7/' + str(
-            subject_id) + '/' + tasks_name + '/'
-        if not os.path.exists(save_model_path):
-            os.makedirs(save_model_path)
+            save_result_path = '../../../Result/GANs/WGan-GP-Signal-VERSION7/' + str(
+                subject_id) + '/' + tasks_name + '/'
+            if not os.path.exists(save_result_path):
+                os.makedirs(save_result_path)
 
-        data, n_chans = get_data(data_load_path=data_load_path,
-                                 time_sample=time_sample,
-                                 window_stride_samples=window_stride_samples,
-                                 mapping=task_mapping,
-                                 pick_channels=all_channels
-                                 )
+            save_model_path = '../../../Model_Params/GANs/WGan-GP-Signal-VERSION7/' + str(
+                subject_id) + '/' + tasks_name + '/'
+            if not os.path.exists(save_model_path):
+                os.makedirs(save_model_path)
 
-        #########################
-        # Running params        #
-        #########################
+            data, n_chans = get_data(data_load_path=data_load_path,
+                                     time_sample=time_sample,
+                                     window_stride_samples=window_stride_samples,
+                                     mapping=task_mapping,
+                                     pick_channels=all_channels
+                                     )
 
-        batchsize = 64
-        epochs = 350
+            #########################
+            # Running params        #
+            #########################
 
-        net = WGANGP(subject=subject_id,
-                     n_epochs=epochs,
-                     batch_size=batchsize,
-                     time_sample=time_sample,
-                     channels=n_chans,
-                     sample_interval=400,
-                     result_path=save_result_path,
-                     )
+            batchsize = 64
+            epochs = 500
 
-        net.train(data, save_model_path=save_model_path)
+            net = WGANGP(subject=subject_id,
+                         n_epochs=epochs,
+                         batch_size=batchsize,
+                         time_sample=time_sample,
+                         channels=n_chans,
+                         sample_interval=400,
+                         result_path=save_result_path,
+                         )
+
+            net.train(data, save_model_path=save_model_path)
 
 print("end")
 
