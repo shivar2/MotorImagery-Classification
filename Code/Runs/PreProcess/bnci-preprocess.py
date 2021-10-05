e760127e91e6d61caf1ef72d22c7a13fb33a28ad
shiva roshanravan <shiva_roshanravan@yahoo.com>
2021-10-04 20:10:35 -0700

    Update bnci-preprocess.py

--- Code/Runs/PreProcess/bnci-preprocess.py
+++ Code/Runs/PreProcess/bnci-preprocess.py
@@ -4,7 +4,7 @@ set_mne_path(dataset_name="BNCI", mne_data_path='../../../Data/mne_data')
 for subject_id in range(1, 10):
 
     dataset = load_data_moabb(dataset_name="BNCI2014001", subject_id=subject_id)
-    dataset = stdmax_normalize_preprocess(dataset, low_cut_hz=None, high_cut_hz=38.)
+    dataset = zmax_normalize_preprocess(dataset, low_cut_hz=None, high_cut_hz=38.)
     # dataset = load_preprocessed_data(data_path='../../../Data/Real_Data/BCI/bnci-raw/0-38/',
     #                                  subject_id=subject_id)
 
